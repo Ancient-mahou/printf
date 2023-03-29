da3698039c636ea4fc0c0f0f351e6713f3bafb7e
Mibonacci <74980532+Mibonacci@users.noreply.github.com>
2023-03-29 02:22:26 +0100

    HHHHH

--- precision.c
+++ precision.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include "precision.h"
 
 /**
  * get_precision - Calculates the precision for printing
@@ -8,30 +9,23 @@
  * @list: list of arguments.
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
+int handle_precision(char **format, va_list args)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
-
-	if (format[curr_i] != '.')
-		return (precision);
-	precision = 0;
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-	*i = curr_i - 1;
-	return (precision);
+    int precision = -1;
+    if (**format == '.')
+    {
+        (*format)++;
+        if (isdigit(**format))
+        {
+            precision = atoi(*format);
+            while (isdigit(**format))
+                (*format)++;
+        }
+        else if (**format == '*')
+        {
+            precision = va_arg(args, int);
+            (*format)++;
+        }
+    }
+    return precision;
 }
